ba587a9f3527278855fd2c9087419a84bfed0e66
laurabpaulsen <202005791@post.au.dk>
2024-04-23 12:38:47 +0200

    add loop over different values of c

--- simulate_data.R
+++ simulate_data.R
@@ -1,22 +1,33 @@
 library(tidyverse)
 source("simulation_functions.R")
 
-# setup experimental design
-experiment <- full_experiment()
+# create a data folder
+dir.create("data")
 
-# extract the attributes of the stimuli
-stimuli <- experiment %>% select("Var1", "Var2", "Var3", "Var4", "Var5")
 
-# make choices according to the GCM
-agent_choices <- gcm(
+# loop over values of c
+c_values <- seq(0.1, 1, 0.2)
+
+
+for (c in c_values){
+  # setup experimental design
+  experiment <- full_experiment()
+  
+  # extract the attributes of the stimuli
+  stimuli <- experiment %>% select("Var1", "Var2", "Var3", "Var4", "Var5")
+  
+  # make choices according to the GCM
+  agent_choices <- gcm(
     w = c(1,1,1,1,1),
-    c = 1,
+    c = c,
     stimuli = stimuli,
     category = experiment$danger
-    )
-
-# add the choices to the dataframe
-experiment$choice <- agent_choices
-
-print(experiment)
+  )
   
+  # add the choices to the dataframe
+  experiment$choice_danger <- agent_choices
+
+  # save the results
+    saveRDS(experiment, file = paste0("data/c_", c, ".rds"))
+
+}
