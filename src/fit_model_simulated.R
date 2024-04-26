0075d17f59b1699e484abbfb511d3dc36f42b276
laurabpaulsen <202005791@post.au.dk>
2024-04-26 10:32:41 +0200

    update to match new version of stan model

--- src/fit_model_simulated.R
+++ src/fit_model_simulated.R
@@ -1,70 +1,74 @@
 # This script runs the simple bayes model on the data from 
-# Simonsen et al and saves the output
+# Simonsen et al and saves the output.
+# Assumes the presence of a Stan model file called "GCM_categorisation.stan".
+# Reads in the simulated data files created by simulate_data.R.
+# Fits the GCM Stan model to the simulated data and runs the Bayesian inference process.
 
-pacman::p_load(cmdstanr, tidyverse)
+# create a model fit folder
+dir.create("fits")
 
+# Ensuring the cmdstanr packages are loaded
+if (!requireNamespace("cmdstanr", quietly = TRUE)) {
+  install.packages("cmdstanr")
+}
+
+library(pacman)
+
+pacman::p_load(cmdstanr,
+               tidyverse)
+
+# Defining the sequence of 'c' parameter values for which the model should be run
 c_values <- seq(0.1, 1, 0.2)
 
-for (c in c_values){
-    # setup experimental design
-    data <- readRDS(paste0("data/c_", c, ".rds"))
 
-    n_trials <- nrow(data)
-    attributes <- data %>% select(c("Var1", "Var2", "Var3", "Var4", "Var5"))
-    categories <- data$danger
-    choices <- data$choice_danger
-
-    model <- cmdstan_model("GCM_categorisation.stan")
-
-    fit <- model$sample(
-        data = list(
-            N = n_trials,
-            attributes = as.matrix(attributes),
-            category = categories,
-            choices = choices
-        ),
-        chains = 4,
-        iter_warmup = 1000,
-        iter_sampling = 1000
-    )
-
+# Looping over each 'c' value to fit the model on the respective simulated dataset
+for (c in c_values) {
+  # Read the simulated data from the appropriate RDS file
+  data <- readRDS(paste0("data/c_", c, ".rds"))
+  
+  # -- Commenting out for now, doing it below directly in fit code -- 
+  
+  # Extract the number of trials, the stimulus attributes, categories, and choices
+  #n_trials <- nrow(data)
+  #attributes <- as.matrix(data %>% select(Var1, Var2, Var3, Var4, Var5))
+  #categories <- data$danger
+  #choices <- data$choice_danger
+  
+  # Load the Stan model
+  model <- cmdstan_model("GCM_categorisation.stan")
+  
+  # Run the Stan model using the cmdstanr 'sample' method
+  fit <- model$sample(
+    data = list(
+      # Not modeling nutritious right now, just doing danger
+      ntrials = nrow(data),       # number of stimuli/trials
+      nfeatures = 5,              # number of features per stimulus
+      cat_one = data$danger,      # binary values indicating dangerous (1) or not dangerous (0)
+      y = data$choice_danger,     # decision outcomes from participants
+      obs = as.matrix(data %>% select(Var1,
+                                       Var2,
+                                       Var3,
+                                       Var4,
+                                       Var5)), # Features matrix
+      b = 0.5, # Initial bias for cat one over two, adjust maybe?
+      w_prior_values = rep(1,5), # uniform prior for the feature weights for now
+      c_prior_values = c(0,1) # m and sd for the scaling parameter
+    ),
+    
+    chains = 4,
+    parallel_chains = 4,
+    iter_warmup = 1000,
+    iter_sampling = 1000,
+    refresh = 0  # Set to 0 to not print progress
+  )
+  
+  # Save the posterior samples from the Stan model fit
+  fit$save_object(file = paste0("fits/model_fit_c_", c, ".rds"))
+  
+  # Save a summary of the fit to CSV
+  fit_summary <- fit$summary()
+  write.csv(fit_summary, file = paste0("fits/fit_summary_c_", c, ".csv"))
+  
+  # Print a message indicating completion of this iteration
+  cat("Finished fitting model for c =", c, "\n")
 }
-
-
-"""
-
-
-
-first_rating_matrix <- matrix(NA, nrow = n_trials, ncol = n_subjects)
-group_rating_matrix <- matrix(NA, nrow = n_trials, ncol = n_subjects)
-second_rating_matrix <- matrix(NA, nrow = n_trials, ncol = n_subjects)
-
-# Fill in the matrices with ratings
-for (i in 1:length(unique_subjects)) {
-  subject_data <- data[data$ID == unique_subjects[i], ]
-  first_rating_matrix[, i] <- subject_data$FirstRating
-  group_rating_matrix[, i] <- subject_data$GroupRating
-  second_rating_matrix[, i] <- subject_data$SecondRating
-}
-
-
-simple_betabayes <- cmdstan_model()
-
-fit <- simple_betabayes$sample(
-  data = list(N = n_trials,
-              N_subj = n_subjects,
-              lower_bound = 1,
-              upper_bound = 8,
-              first_rating = first_rating_matrix,
-              group_rating = group_rating_matrix,
-              second_rating = second_rating_matrix
-  ),
-  chains = 4,
-  thin = 2,
-  iter_warmup = 1000
-)
-
-# save the posterior
-fit$save_object(file = )
-
-"""
