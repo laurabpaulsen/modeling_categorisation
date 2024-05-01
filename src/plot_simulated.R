aed91116c0f955c6faf05ea7670f73f823fc0ac2
laurabpaulsen <202005791@post.au.dk>
2024-05-01 16:08:38 +0200

    no more plotting madness

--- src/plot_simulated.R
+++ src/plot_simulated.R
@@ -1,6 +1,6 @@
 
 library(pacman)
-pacman::p_load(posterior, ggplot2, gridExtra, tidyverse)
+pacman::p_load(posterior, ggplot2, gridExtra, tidyverse, ggridges)
 
 dir.create("fig", showWarnings = FALSE)
 
@@ -14,84 +14,43 @@ list_of_weights <- list(
 )
 
 
-for (c in c_values) {
 
+dataframe <- tibble()
+
+for (c in c_values) {
   for (w in list_of_weights) {
       # Read the simulated data from the appropriate rds file
       fit <- readRDS(paste0("fits/model_fit_c_", c, "_w_", paste0(w, collapse = "_"), ".rds"))
 
-      # plot posterior distribution
       # extract the posterior
       posterior <- fit$draws()
 
       fit_df <- as_draws_df(posterior)
-      
 
-      ggplot(fit_df) +
-        # plot the prior density
-        geom_density(aes(x = prior_c), fill = "skyblue", alpha = 0.5) +
-        
-        # plot the posterior density
-        geom_density(aes(x = posterior_c), fill = "red", alpha = 0.5) +
-
-        # add a vertical line for the true value of c
-        geom_vline(xintercept = c, linetype = "dashed") +
-        
-        labs(title = paste0("Prior and posterior density for c = ", c),
-              x = "c",
-              y = "Density") +
-
-        xlim(0, 5) +
-
-        theme_bw()
-    
-      # save the plot
-      ggsave(paste0("fig/posteriorc_c", c, "_w_", paste0(w, collapse = "_"), ".png"))
-
-      plot1 <- fit_df %>% ggplot() +
-        geom_density(aes(x = `prior_w[1]`), fill = "skyblue", alpha = 0.5) +
-        geom_density(aes(x = `posterior_w[1]`), fill = "red", alpha = 0.5) +
-        geom_vline(xintercept = w[1], linetype = "dashed") +
-        labs(x = "weight 1",
-            y = "Density") +
-        theme_bw()
-
-      plot2 <- fit_df %>% ggplot() +
-        geom_density(aes(x = `prior_w[2]`), fill = "skyblue", alpha = 0.5) +
-        geom_density(aes(x = `posterior_w[2]`), fill = "red", alpha = 0.5) +
-        geom_vline(xintercept = w[2], linetype = "dashed") +
-        labs(x = "weight 2",
-            y = "Density") +
-        theme_bw()
-
-      plot3 <- fit_df %>% ggplot() +
-        geom_density(aes(x = `prior_w[3]`), fill = "skyblue", alpha = 0.5) +
-        geom_density(aes(x = `posterior_w[3]`), fill = "red", alpha = 0.5) + 
-        geom_vline(xintercept = w[3], linetype = "dashed") +
-        labs(x = "weight 3",
-            y = "Density") +
-        theme_bw()
-      
-      plot4 <- fit_df %>% ggplot() +
-        geom_density(aes(x = `prior_w[4]`), fill = "skyblue", alpha = 0.5) +
-        geom_density(aes(x = `posterior_w[4]`), fill = "red", alpha = 0.5) + 
-        geom_vline(xintercept = w[4], linetype = "dashed") +
-        labs(x = "weight 4",
-            y = "Density") +
-        theme_bw()
-      
-      plot5 <- fit_df %>% ggplot() +
-        geom_density(aes(x = `prior_w[5]`), fill = "skyblue", alpha = 0.5) +
-        geom_density(aes(x = `posterior_w[5]`), fill = "red", alpha = 0.5) + 
-        labs(x = "weight 5",
-            y = "Density") +
-        theme_bw()
+      fit_df$c <- c
+      fit_df$w1 <- w[1]
+      fit_df$w2 <- w[2]
+      fit_df$w3 <- w[3]
+      fit_df$w4 <- w[4]
+      fit_df$w5 <- w[5]
 
 
-      combined <- grid.arrange(plot1, plot2, plot3, plot4, plot5,  ncol = 3, nrow = 2)
+      fit_df$c <- as.factor(fit_df$c)
+      fit_df$weights_string <- paste(w[1], w[2], w[3], w[4], w[5])
 
-      # save the plot
-      ggsave(paste0("fig/posteriorsweights_c_", c, "_w_", paste0(w, collapse = "_"), ".png"), plot = combined)
-
+      dataframe <- bind_rows(dataframe, fit_df)
   }
 }
+
+
+ggplot(dataframe) +
+  geom_density_ridges(aes(x = posterior_c, y = c), alpha = 0.5) +
+  # add a line for the value of true c in each plot
+  facet_wrap(~weights_string) +
+  labs(title = "Posterior density for c",
+        x = "Estimated c",
+        y = "True c") +
+  xlim(0, 5) +
+  theme_bw()
+  
+ggsave("fig/recovery_of_c.png")
